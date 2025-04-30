425af1528365723ae5c2197421c6f3407ebda564
HamzaLaouzi <hlaouzi@uoc.edu>
2025-04-30 20:04:36 +0200

    Migración a Laravel con correcciones de campos y estructura

--- Desktop/Laravel/src/app/Models/User.php
+++ Desktop/Laravel/src/app/Models/User.php
@@ -1,16 +1,11 @@
-<?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -23,11 +18,6 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
